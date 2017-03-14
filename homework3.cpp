aa2487a41e8004e87df0f3989f0651ffbc082475
dbperry2 <dbperry2@asu.edu>
2017-03-13 18:17:09 -0700

    Done for the day

--- homework3.cpp
+++ homework3.cpp
@@ -2,9 +2,73 @@
 // Created by dbperry2 on 3/13/17.
 //
 #include <iostream>
+using namespace std;
+
+/*
+ * S -> A C
+ * S -> B D
+ * A -> a A
+ * A -> #
+ * B -> a B
+ * B -> a
+ * C -> c
+ * D -> d
+ *
+ *
+ * OR:
+ *
+ * S -> A C | B D
+ * A -> a A | #
+ * B -> a B | a
+ * C -> c
+ * D -> d
+ */
+
+//S -> A C | B D
+bool parse_S()
+{
+    bool parsed = true;
+
+
+    return parsed;
+}
+
+
+//A -> a A | #
+bool parse_A()
+{
+    bool parsed = true;
+
+    return parsed;
+}
+
+//B -> a B | a
+bool parse_B()
+{
+    bool parsed = true;
+
+    return parsed;
+}
+
+//C -> c
+bool parse_C()
+{
+    bool parsed = true;
+
+    return parsed;
+}
+
+//D -> d
+bool parse_D()
+{
+    bool parsed = true;
+
+    return parsed;
+}
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
