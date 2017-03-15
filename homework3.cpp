baaa65f737d2f23959939d51ba922bdc4d525fab
dbperry2 <dbperry2@asu.edu>
2017-03-15 12:57:30 -0700

    Working on part 1; plenty of errors, but basic framework is there

--- homework3.cpp
+++ homework3.cpp
@@ -2,7 +2,10 @@
 // Created by dbperry2 on 3/13/17.
 //
 #include <iostream>
+#include "lexer.h"
+
 using namespace std;
+LexicalAnalyzer lexer;
 
 /*
  * S -> A C
@@ -22,53 +25,120 @@ using namespace std;
  * B -> a B | a
  * C -> c
  * D -> d
+ *
+ * terminals: a c d
  */
+
+bool parse_A();
+bool parse_B();
+bool parse_C();
+bool parse_D();
 
 //S -> A C | B D
 bool parse_S()
 {
-    bool parsed = true;
 
-
-    return parsed;
+    if (parse_A())
+    {
+        if (parse_C())
+        {
+            cout << "S -> AC" << endl;
+            return true;
+        }
+        else
+        {
+            cout << "S -> A ~C" << endl;
+            return false;
+        }
+    }
+    else if (parse_B())
+    {
+        if(parse_D())
+        {
+            cout << "S -> BD" << endl;
+            return true;
+        }
+        else
+        {
+            cout << "S -> B ~D" << endl;
+            return false;
+        }
+    }
+    else
+    {
+        cout << "S -> ~(A || B) ... " << endl;
+        return false;
+    }
 }
-
 
 //A -> a A | #
 bool parse_A()
 {
-    bool parsed = true;
+    Token tok;
 
-    return parsed;
+    tok = lexer.GetToken();
+    if(tok.token_type == )
+    {
+        tok = lexer.GetToken();
+        if((tok.Token_type == LPAREN) || tok.token_type == DOT)
+        {
+            //if condition checks FIRST(A)
+            //technically we can omit the previous if statement
+            //and go straight to parseA
+            //this way detects error as soon as possible
+            lexer.ungettoken
+            if(parseA
+                    gettoken
+                if RPAREN
+                    cout << "( A )"
+            return true
+            else
+            unget
+            return false
+            else
+            return false
+        }
+        else
+            return false
+    }
+    else if DOT
+        return true
+    else
+        unget
+        cout << "A -> ~( A ) and A -> ." << endl;
+    return true;
+
 }
+
 
 //B -> a B | a
 bool parse_B()
 {
-    bool parsed = true;
-
-    return parsed;
+    /*
+    Similar to A
+    */
 }
 
 //C -> c
 bool parse_C()
 {
-    bool parsed = true;
-
-    return parsed;
+    
 }
 
 //D -> d
 bool parse_D()
 {
-    bool parsed = true;
-
-    return parsed;
+    
 }
 
 
 int main()
 {
+    parse_S();
 
+    Token t = lexer.GetToken();
+    if(t.token_type != END_OF_FILE)
+        cout << "FAILURE!" << endl;
+    
     return 0;
 }
